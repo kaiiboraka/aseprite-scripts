08d25d6f7b7a3bb64c2a601b31f92a95755b3997
kaiiboraka <kaiiboraka@gmail.com>
2021-05-11 18:22:13 -0600

    Update color-compliment.lua

--- color-compliment.lua
+++ color-compliment.lua
@@ -1,16 +1,23 @@
 -- Aseprite Script to show color compliment of given color
 -- Written by aquova, 2018
 -- https://github.com/aquova/aseprite-scripts
+-- MODIFIED by kaiiboraka, 2021
+-- https://github.com/kaiiboraka/aseprite-scripts
 
 -- Open dialog, ask user for color
 function userInput()
-    local dlg = Dialog()
+    local dlg = Dialog
+	{
+		title="Complimentary Color"
+	}
     -- Creates a starting color of black
     local defaultColor = Color{r=0, g=0, b=0, a=255}
-    dlg:color{ id="color", label="Choose a color", color=defaultColor }
+	local colorfg = app.fgColor
+	local colorbg = app.bgColor
+    dlg:color{ id="color", label="Choose a color", color=colorfg }
     dlg:button{ id="ok", text="OK" }
     dlg:button{ id="cancel", text="Cancel" }
-    dlg:show()
+    --dlg:show()
 
     return dlg.data
 end
@@ -22,19 +29,46 @@ function generateCompliment(color)
 end
 
 -- Generates the color gradiants and displays them
-function showOutput(color)
-    local dlg = Dialog()
-    local newColor = generateCompliment(color)
-    dlg:color{ color=newColor }
+function showOutput(oldColor)
+    local dlg = Dialog
+	{
+		title="Complimentary Color"
+	}
+    local newColor = generateCompliment(oldColor)
+    dlg:shades
+	{
+		id='hue', label='Original Hue:',
+		colors = {oldColor},
+		onclick = function(ev) app.fgColor = ev.color
+		--onclick = function(ev) app.bgColor = ev.color
+		end
+	}
+	
+	dlg:shades
+	{
+		id='hue', label='Complimentary Hue:',
+		colors = {newColor},
+		onclick = function(ev) app.bgColor = ev.color
+		--onclick = function(ev) app.bgColor = ev.color
+		end
+	}
+	
     dlg:button{ id="ok", text="OK" }
-    dlg:show()
+    dlg:show{wait=false}
 end
 
 -- Run script
 do
     local userColor = userInput()
-    if userColor.ok then
-        local c = Color{r=userColor.color.red, g=userColor.color.green, b=userColor.color.blue, a=userColor.color.alpha}
+    --if userColor.ok then
+        local c = Color
+		{
+			r = userColor.color.red, 
+			g = userColor.color.green, 
+			b = userColor.color.blue, 
+			a = userColor.color.alpha
+		}
+		
         showOutput(c)
-    end
+    --end
 end
